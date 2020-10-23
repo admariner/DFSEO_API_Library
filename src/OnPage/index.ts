3329de298bca8d2a47277e392ff7017da8721842
unknown <michaelsorensenatwork@gmail.com>
2020-10-23 09:11:22 -0700

    added more types

--- src/OnPage/index.ts
+++ src/OnPage/index.ts
@@ -1,7 +1,8 @@
 import { DFSEO } from "..";
 import { IOnPage_Task_Post_Request, IOnPage_Task_Post_Response } from "../typings/onPage_api/task_post";
 import { IOnPage_Summary } from "../typings/onPage_api/summary";
-import { IOnPage_Pages_Request } from "../typings/onPage_api/pages";
+import { IOnPage_Pages_Request, IOnPage_Pages_Response } from "../typings/onPage_api/pages";
+import { IOnPage_Pages_By_Resource_Request } from "../typings/onPage_api/pages_by_resource";
 
 export class OnPage {
 	constructor(private DFSEO: DFSEO) {}
@@ -21,7 +22,7 @@ export class OnPage {
 		});
 	}
 
-	public pages(data: IOnPage_Pages_Request[]) {
+	public pages(data: IOnPage_Pages_Request[]): Promise<IOnPage_Pages_Response> {
 		return this.DFSEO.fetch({
 			method: "POST",
 			url: "on_page/pages",
@@ -29,7 +30,7 @@ export class OnPage {
 		});
 	}
 
-	public pagesByResource(data: any[]) {
+	public pagesByResource(data: IOnPage_Pages_By_Resource_Request[]): Promise<IOnPage_Pages_Response> {
 		return this.DFSEO.fetch({
 			method: "POST",
 			url: "on_page/pages_by_resource",
