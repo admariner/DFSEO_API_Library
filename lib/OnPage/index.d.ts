21323901c4cf437da0cb1c3ddb9c2d6db793b730
unknown <michaelsorensenatwork@gmail.com>
2020-10-23 09:12:46 -0700

    forgot to build

--- lib/OnPage/index.d.ts
+++ lib/OnPage/index.d.ts
@@ -1,14 +1,15 @@
 import { DFSEO } from "..";
 import { IOnPage_Task_Post_Request, IOnPage_Task_Post_Response } from "../typings/onPage_api/task_post";
 import { IOnPage_Summary } from "../typings/onPage_api/summary";
-import { IOnPage_Pages_Request } from "../typings/onPage_api/pages";
+import { IOnPage_Pages_Request, IOnPage_Pages_Response } from "../typings/onPage_api/pages";
+import { IOnPage_Pages_By_Resource_Request } from "../typings/onPage_api/pages_by_resource";
 export declare class OnPage {
     private DFSEO;
     constructor(DFSEO: DFSEO);
     taskPost(data: IOnPage_Task_Post_Request[]): Promise<IOnPage_Task_Post_Response>;
     summary(id: string): Promise<IOnPage_Summary>;
-    pages(data: IOnPage_Pages_Request[]): Promise<any>;
-    pagesByResource(data: any[]): Promise<any>;
+    pages(data: IOnPage_Pages_Request[]): Promise<IOnPage_Pages_Response>;
+    pagesByResource(data: IOnPage_Pages_By_Resource_Request[]): Promise<IOnPage_Pages_Response>;
     resources(data: any[]): Promise<any>;
     duplicate_tags(data: any[]): Promise<any>;
     duplicate_content(data: any[]): Promise<any>;
